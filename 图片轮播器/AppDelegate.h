80d3a5d3fbb0a22824ecd6efdcce7dfe6770f51a
Rulee1012 <946108596@qq.com>
2016-04-01 15:12:47 +0800

    1
    1

---
+++ 图片轮播器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  图片轮播器
+//
+//  Created by mac on 16/4/1.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 图片轮播器/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  图片轮播器
+//
+//  Created by mac on 16/4/1.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
